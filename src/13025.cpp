641d1283c24e080232d3172a3c436b8f187c6ac1
Jack Fawthorpe <jfa93@uclive.ac.nz>
2023-10-15 22:05:35 +1300

    Back to the past

---
+++ src/13025.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+
+int main()
+{
+    #ifndef ONLINE_JUDGE 
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    cout << "May 29, 2013 Wednesday\n";
+}
